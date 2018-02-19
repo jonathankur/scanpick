5014a4acf265c4705d8a2d136d0179f2158fc183
jonathankur <30434945+jonathankur@users.noreply.github.com>
2018-02-19 13:33:18 +0800

    4
    4

--- build/main.js
+++ build/main.js
@@ -171,11 +171,14 @@ var PickPage = (function () {
     PickPage.prototype.goOrd = function (o) {
         this.navCtrl.push(__WEBPACK_IMPORTED_MODULE_3__picker_picker__["a" /* PickerPage */], { 'orderno': o.id });
     };
+    PickPage.prototype.tryOrd = function (o) {
+        this.navCtrl.push(__WEBPACK_IMPORTED_MODULE_3__picker_picker__["a" /* PickerPage */], { 'orderno': o });
+    };
     PickPage.prototype.scanBar = function () {
         var _this = this;
         this.barcodeScanner.scan().then(function (barcodeData) {
             if (!barcodeData.cancelled) {
-                _this.goOrd(barcodeData.text);
+                _this.tryOrd(barcodeData.text);
             }
         }, function (err) {
         });
@@ -186,9 +189,10 @@ PickPage = __decorate([
     Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
         selector: 'page-pick',template:/*ion-inline-start:"/var/www/html/ionic/scanpick/src/pages/pick/pick.html"*/'<!--\n  Generated template for the StockPage page.\n\n  See http://ionicframework.com/docs/components/#navigation for more info on\n  Ionic pages and navigation.\n-->\n<ion-header>\n\n  <ion-navbar>\n    <ion-title>Scan Pick</ion-title>\n  </ion-navbar>\n\n</ion-header>\n\n\n<ion-content padding>\n<div *ngIf="company ==0">\n<ion-item>\n<button ion-button full color="primary" (click)="scanBar()">Scan Order #</button>\n</ion-item>\n<ion-row>\n<ion-col col-12 text-center>\n<h2>Select Company</h2>\n</ion-col>\n</ion-row>\n\n\n		<ion-list no-lines text-wrap >\n			<ion-item *ngFor="let c of companies"  class="borderBottomGainsboroAlpha">\n<button ion-button (click)="goComp(c)" block color="primary">{{ c.name }}</button>\n			</ion-item>\n		</ion-list>\n</div>\n<div *ngIf="company > 0">\n<ion-row>\n<ion-col col-12 text-center>\n<h3>{{ companyname }}</h3>\n<button ion-button color="danger" block (click)="goBack()">Go Back</button>\n</ion-col>\n</ion-row>\n		<ion-list no-lines text-wrap >\n			<ion-item *ngFor="let o of orders"  class="borderBottomGainsboroAlpha">\n<button ion-button style="height:60px" (click)="goOrd(o)" block color="primary" text-wrap><h3>[{{ o.id }}] {{ o.name }}</h3></button>\n			</ion-item>\n		</ion-list>\n</div>\n</ion-content>\n'/*ion-inline-end:"/var/www/html/ionic/scanpick/src/pages/pick/pick.html"*/,
     }),
-    __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["g" /* NavController */], __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["h" /* NavParams */], __WEBPACK_IMPORTED_MODULE_2__providers_connect__["a" /* Connect */], __WEBPACK_IMPORTED_MODULE_0__angular_core__["P" /* NgZone */], __WEBPACK_IMPORTED_MODULE_0__angular_core__["k" /* ChangeDetectorRef */], __WEBPACK_IMPORTED_MODULE_4__ionic_native_barcode_scanner__["a" /* BarcodeScanner */]])
+    __metadata("design:paramtypes", [typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["g" /* NavController */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["g" /* NavController */]) === "function" && _a || Object, typeof (_b = typeof __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["h" /* NavParams */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["h" /* NavParams */]) === "function" && _b || Object, typeof (_c = typeof __WEBPACK_IMPORTED_MODULE_2__providers_connect__["a" /* Connect */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_2__providers_connect__["a" /* Connect */]) === "function" && _c || Object, typeof (_d = typeof __WEBPACK_IMPORTED_MODULE_0__angular_core__["P" /* NgZone */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_0__angular_core__["P" /* NgZone */]) === "function" && _d || Object, typeof (_e = typeof __WEBPACK_IMPORTED_MODULE_0__angular_core__["k" /* ChangeDetectorRef */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_0__angular_core__["k" /* ChangeDetectorRef */]) === "function" && _e || Object, typeof (_f = typeof __WEBPACK_IMPORTED_MODULE_4__ionic_native_barcode_scanner__["a" /* BarcodeScanner */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_4__ionic_native_barcode_scanner__["a" /* BarcodeScanner */]) === "function" && _f || Object])
 ], PickPage);
 
+var _a, _b, _c, _d, _e, _f;
 //# sourceMappingURL=pick.js.map
 
 /***/ }),
